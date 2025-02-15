73968c19323305e54c163bcdff18f1f1d705d6fa
EEHiatte <elijah.hiatte@gmail.com>
2025-02-14 19:25:47 -0600

    Added entrypoint to begin game flow logic from.

---
+++ Assets/Scripts/GameFlow/EntryPoint.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class EntryPoint : MonoBehaviour
+{
+    void Start()
+    {
+        // TODO: Start game logic and flow here, don't use Start and Update methods
+        // on other objects unless they aren't tied to the gameplay flow.
+    }
+}

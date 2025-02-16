629b7fa38e8c0b2328f9ef75705c87ee6f538345
brhinier <b.rhinier@gmail.com>
2025-02-16 15:58:35 -0600

    Acid tower sound

--- Assets/Scripts/Tower/AcidTower.cs
+++ Assets/Scripts/Tower/AcidTower.cs
@@ -4,6 +4,7 @@ public class AcidTower : DamagingTowerBase
 {
     [SerializeField] private ParticleSystem _explosionEffect;
     [SerializeField] private PassiveDamagingObject _poisonObject;
+    [SerializeField] private AudioSource _slimeAudio;
     
     public override bool OnTryDamage(DamageInfo damageInfo)
     {
@@ -18,6 +19,7 @@ public class AcidTower : DamagingTowerBase
         var spawnPos = new Vector3(target.transform.position.x, target.transform.position.y, 0f);
         var randomRotation = Quaternion.Euler(0f, 0f, Random.Range(0f, 360f));
         var acid = Instantiate(_poisonObject, spawnPos, randomRotation);
+        _slimeAudio.Play();
         return true;
     }
 }

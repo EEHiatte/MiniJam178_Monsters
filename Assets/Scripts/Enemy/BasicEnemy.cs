f1b73ded7ab67c9be63f248883a5b67df643c1d2
brhinier <b.rhinier@gmail.com>
2025-02-16 17:19:13 -0600

    Enemies now show when poisoned

--- Assets/Scripts/Enemy/BasicEnemy.cs
+++ Assets/Scripts/Enemy/BasicEnemy.cs
@@ -1,11 +1,7 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Splines;
-using UnityEngine.Splines.Interpolators;
 
 public class BasicEnemy : MonoBehaviour
 {
@@ -37,15 +33,24 @@ public class BasicEnemy : MonoBehaviour
     private bool slowed = false;
     
     public float CurrentSpeed => Speed * _speedModifier;
-    private PoisonHelper _poisonHelper = new PoisonHelper();
+    private PoisonHelper _poisonHelper;
+    [SerializeField] private ParticleSystem _poisonTrail;
 
     public class PoisonHelper
     {
         private float _tickTimer;
         private Queue<int> _poisonTicks = new();
+        private ParticleSystem _poisonParticles;
+
+        public PoisonHelper(ParticleSystem poisonParticles)
+        {
+            _poisonParticles = poisonParticles;
+        }
         
         public void AddTicks(int ticks, int damageAmount)
         {
+            if(!_poisonParticles.isPlaying) _poisonParticles.Play();
+            
             for(int i = 0; i < ticks; ++i)
                 _poisonTicks.Enqueue(damageAmount);
         }
@@ -61,13 +66,16 @@ public class BasicEnemy : MonoBehaviour
             if (_tickTimer <= 0f && _poisonTicks.Count > 0)
             {
                 _tickTimer = .5f;
-                return _poisonTicks.Dequeue();
+                var damage = _poisonTicks.Dequeue();
+
+                if (_poisonTicks.Count == 0)
+                    _poisonParticles.Stop();
+                
+                return damage;
             }
 
             return 0;
         }
-        
-        
     }
 
     void Start()
@@ -76,6 +84,8 @@ public class BasicEnemy : MonoBehaviour
         splineAnimator.Completed += EndOfPath;
         GetComponent<SpriteRenderer>().material.SetColor("_Tint", GetComponent<SpriteRenderer>().color);
         tempColor = GetComponent<SpriteRenderer>().color;
+        _poisonHelper = new PoisonHelper(_poisonTrail);
+        _poisonTrail.Stop();
     }
 
     // Update is called once per frame
